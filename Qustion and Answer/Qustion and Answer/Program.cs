27ffb2b817050f9484e9a41edf1735749824967b
Muhammed Adam (1000065402) <ADAMM7@student.op.ac.nz>
2019-05-03 11:04:14 +1200

    Sorry for being STOOOPID

---
+++ Qustion and Answer/Qustion and Answer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qustion_and_Answer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is where wer start coding
+        }
+    }
+}
