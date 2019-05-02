381301c22c76a0884f23828a6ff42edb2b503272
Georgette Watkins (1000060043) <WATKGL1@student.op.ac.nz>
2019-05-03 11:13:04 +1200

    Added lab 10 code

--- Qustion and Answer/Qustion and Answer/Program.cs
+++ Qustion and Answer/Qustion and Answer/Program.cs
@@ -11,6 +11,33 @@ namespace Qustion_and_Answer
         static void Main(string[] args)
         {
             //This is where wer start coding
+	    //
+	    //difine the variables
+            int witch, heads = 0, tails = 0, i;
+            //char headsH, tailsT;
+
+            Random rand = new Random();
+
+            //for loop
+
+            for (i =1; i < 1001; i++)
+            {
+                witch = rand.Next(2);
+                if (witch == 1)
+                {
+                    heads = heads + 1;
+                }
+                else
+                {
+                    tails = tails + 1;
+                }
+
+            }
+
+            //displays the amount of times H and the amount of times T was thrown
+
+            Console.WriteLine($"heads has been fliped = {heads} times. Tails tails has been fliped = {tails} times.");
+            Console.ReadLine();
         }
     }
 }
